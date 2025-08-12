bb9291ca3e35064b5195eb79838aec497d12c9d6
joshnaTheExplorer <dandikajoshna2@gmail.com>
2025-08-12 15:49:19 +0530

    first commit

---
+++ phython.py
@@ -0,0 +1,18 @@
+print("dandika joshna")
+import sys
+print(sys.version)
+print("josh")
+print("good moning")
+#exit()--- ntg gets executes after this
+# syntax
+if 10>5 :
+    print("greater")
+    if 10<5:
+        print("lower")
+x=5
+print(x)
+#comments
+""" multiline comment"""
+print(type(x))
+myvar=("welcome")
+print(myvar)
